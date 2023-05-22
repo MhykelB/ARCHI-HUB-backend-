ac042e6f77a0a84b8d4f54f151e462c2900ee707
unknown <micheal.badmus@gmail.com>
2023-05-22 20:01:03 +0100

    ghblnnmnnnmg

--- hubApp.js
+++ hubApp.js
@@ -1,10 +1,10 @@
 //modules
 
 //swagger
-// const swaggerUI = require("swagger-ui-express");
+const swaggerUI = require("swagger-ui-express");
 // const YAML = require("yamljs");
 // const swaggerDoc = YAML.load("./swagger.yaml");
-// const swaggerDoc = require("./swagger.json");
+const swaggerDoc = require("./swagger.json");
 
 //express
 const express = require("express");
